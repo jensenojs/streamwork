040a0a49c43c322458449980d56ca170761f8378
Jensen <jensenojs@qq.com>
2022-10-16 21:14:04 +0800

    feat : implement event dispatcher

---
+++ pkg/engine/event_dispatcher.go
@@ -0,0 +1,67 @@
+package engine
+
+import "streamwork/pkg/api"
+
+/**
+ * EventDispatcher is responsible for transporting events from
+ * the incoming queue to the outgoing queues with a grouping strategy.
+ */
+type EventDispatcher struct {
+	fnWrapper          func()      // wrapper function for fn
+	fn                 func() bool // process function, need to specific implementation for user logic
+	downStreamExecutor *OperatorExecutor
+	incomingQueue      *EventQueue
+	outgoingQueues     []*EventQueue
+}
+
+func NewEventDispatcher(downStreamExecutor *OperatorExecutor) *EventDispatcher {
+	return &EventDispatcher{
+		downStreamExecutor: downStreamExecutor,
+	}
+}
+
+// ============================================================
+// implementation of Process
+func (v *EventDispatcher) newProcess() {
+	v.fnWrapper = func() {
+		for {
+			if ok := v.fn(); ok != true {
+				break
+			}
+		}
+	}
+}
+
+func (v *EventDispatcher) Start() {
+	go v.fnWrapper()
+}
+
+func (v *EventDispatcher) runOnce() bool {
+	e := v.takeIncomingEvent()
+	s := v.downStreamExecutor.GetGroupingStrategy()
+	idx := s.GetInstance(e, len(v.outgoingQueues))
+	v.sendOutgoingEvent(e, idx)
+	return true
+}
+
+// ============================================================
+// init and use for incoming/outgoing queues
+func (v *EventDispatcher) takeIncomingEvent() api.Event {
+	e, ok := <-v.incomingQueue.queue
+	if ok {
+		return e
+	}
+	return nil
+}
+
+func (v *EventDispatcher) sendOutgoingEvent(event api.Event, idx int) {
+	v.outgoingQueues[idx].queue <- event
+}
+
+func (v *EventDispatcher) SetIncomingQueue(queue *EventQueue) {
+	v.incomingQueue = queue
+}
+
+func (v *EventDispatcher) SetOutgoingQueue(queues []*EventQueue) {
+	v.outgoingQueues = queues
+}
